aa2be3dadbc67f74b7a6f1e3ce907e4afbb049e1
Uzzwal Dhali <d.uzzwal09@gmail.com>
2023-09-10 05:23:38 +0600

    Improved UI and added dynamic modal for create or edit task

--- client/src/components/ListHeader.js
+++ client/src/components/ListHeader.js
@@ -1,4 +1,8 @@
+import { useState } from 'react'
+import Modal from './Modal'
+
 const ListHeader = ({ listName }) => {
+  const [ showModal, setShowModal ] = useState(false)
 
   const signOut = () => {
     console.log('signOut')
@@ -8,9 +12,10 @@ const ListHeader = ({ listName }) => {
     <div className="list_header">
       <h1>{ listName }</h1>
       <div className="buttons_container">
-        <div className="button create">Add New</div>
-        <div className="button signout" onClick="{signOut}">Sgnout</div>
+        <div className="button create" onClick={() => setShowModal(true)}>Add New</div>
+        <div className="button signout" onClick={signOut}>Sgnout</div>
       </div>
+      {showModal && <Modal option={'create'} setShowModal={setShowModal} />}
     </div>
   );
 }
--- client/src/components/Modal.js
+++ client/src/components/Modal.js
@@ -1,6 +1,71 @@
-const Modal = () => {
+import { useState } from 'react'
+
+const Modal = ({ option, setShowModal }) => {
+  // const option = 'create'
+  const selectedOption = option === 'edit' ? true : false
+  const [data, setData] = useState({
+    user_email: "",
+    title: "",
+    progress: "",
+    date: selectedOption ? "" : new Date()
+  })
+
+  const handleChange = (e) => {
+    const { name, value } = e.target
+
+    setData (data => ({
+      ...data,
+      [name] : value
+    }))
+    console.log(data)
+  }
   return (
-    <div>
+    <div className="overlay">
+      <div className="modal">
+        <div className="header">
+          <div className="title">Let's {option} your task</div>
+          <div className="close" onClick={() => setShowModal(false)}>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 24 24"
+              width="25"
+              height="25"
+            >
+              <g>
+                <path fill="none" d="M0 0h24v24H0z"/>
+                <path d="M12 10.586l4.95-4.95 1.414 1.414-4.95 4.95 4.95 4.95-1.414 1.414-4.95-4.95-4.95 4.95-1.414-1.414 4.95-4.95-4.95-4.95L7.05 5.636z"/>
+              </g>
+            </svg>
+          </div>
+        </div>
+        <div className="body">
+          <form>
+            <input
+              required
+              maxLength="30"
+              placeholder="Task title"
+              name="title"
+              value={data.title}
+              onChange={handleChange}
+            />
+
+            <div className="input_group">
+              <label htmlFor="range">Select current progress</label>
+              <input
+                required
+                type="range"
+                id="range"
+                min="0"
+                max="100"
+                name="progress"
+                value={data.progress}
+                onChange={handleChange}
+              />
+            </div>
+            <input className={option} type="submit" value={option === 'create' ? 'CREATE' : 'UPDATE'} />
+          </form>
+        </div>
+      </div>
     </div>
   );
 }
